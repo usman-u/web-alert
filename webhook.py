1cadc535e557b3e040822f0d1d5648385f2dffd9
usman <unumer@hotmail.com>
2021-02-10 10:32:16 +0000

    completed webhook & update requirements

--- webhook.py
+++ webhook.py
@@ -1,11 +1,24 @@
 from discord_webhook import DiscordWebhook
 from time import ctime
+from bs4 import BeautifulSoup
+import requests
 
-in_stock = False  # boolean value
+URL = "https://www.awd-it.co.uk/xfx-gts-radeon-rx-580-8gb-xxx-gddr5-pci-express-gaming-graphics-card.html"
+page = requests.get(URL, headers = {'User-agent': 'Super Bot Power Level Over 9000'})
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+string = str(soup)
+
+if 'OutOfStock' in string:
+	print("OutOfStock") 
+	in_stock = False
+if "InStock" in string:
+	print ("instock")
+	in_stock = True
 
 time = ctime()
 def discord(msg):
-	# content = msg sent
 	webhook = DiscordWebhook(url='https://discord.com/api/webhooks/808829261666189312/ErkuQx7hoUe-RxC6Og8j1dxpv15x3AAVk4HJB1i96aNP9pCWSUsQAHfaLa0Q0wSKhfu8', content=msg)
 	response = webhook.execute()  # executes webhook
 
